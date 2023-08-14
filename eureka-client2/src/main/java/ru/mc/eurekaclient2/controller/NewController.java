4f44550816a844e5394682389b748421828989f5
Dmitry Blyukherov <d.blukherov@gmail.com>
2023-08-14 14:32:00 +0300

    добавлен eureka-gateway и несколько инстансов client, откорректираваны номера портов на общепринятые

---
+++ eureka-client2/src/main/java/ru/mc/eurekaclient2/controller/NewController.java
@@ -0,0 +1,16 @@
+package ru.mc.eurekaclient2.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/new")
+public class NewController {
+
+    @GetMapping("/name")
+    public String name() {
+        return "HHHHHHHHHHHHHHHHHHHHHHHH";
+    }
+
+}
